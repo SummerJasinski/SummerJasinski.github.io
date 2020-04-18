fc7f7a27629a52d4aaaf6ff337ac93af436d3404
SummerJasinski <38509510+SummerJasinski@users.noreply.github.com>
2020-04-18 15:31:20 -0400

    Update JSScript.js

--- JSScript.js
+++ JSScript.js
@@ -1,1 +1,16 @@
 
+   
+  var button = document.getElementsByClassName("expand-button");
+  var i;
+  for (i = 0; i< button.length; i++){
+     button[i].addEventListener("click",function(){
+     this.classList.toggle("active");
+     var content = this.nextElementSibling;
+     if (content.style.display === "block"){
+     content.style.display = "none";
+     }
+     else { content.style.display = "block"
+     }
+     });
+     }
+ 
